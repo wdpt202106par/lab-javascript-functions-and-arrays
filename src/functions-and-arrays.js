d46320095559ba891fec2764b20472d216bef287
Projetphp <alexandre.capaldi@hotmail.fr>
2021-02-20 12:10:58 +0100

    iteration1-done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,23 @@
 // Iteration #1: Find the maximum
 
+
+function maxOfTwoNumbers(num1, num2) {
+
+  if (num1 > num2)
+
+    console.log(num1);
+
+  else {
+    console.log(num2);
+
+  }
+
+}
+
+maxOfTwoNumbers(4, 7)
+
+
+
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
